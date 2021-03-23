fabf2d20f988806c8bb2d0c4c43e180007909b41
Hector Vigil <vigilescalera.h@gmail.com>
2021-03-23 16:55:39 -0400

    6- State Management in react components

--- App.js
+++ App.js
@@ -9,6 +9,10 @@ import HomeScreen from "./src/screens/HomeScreen";
 import ComponentScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SquareScreen";
+import TextScreen from "./src/screens/TextScreen";
 
 const Stack = new createStackNavigator();
 
@@ -20,6 +24,10 @@ export default function App(){
             <Stack.Screen name="Components" component={ComponentScreen} />
             <Stack.Screen name="List" component={ListScreen}/>
             <Stack.Screen name="Image" component={ImageScreen}/>
+            <Stack.Screen name="Counter" component={CounterScreen}/>
+            <Stack.Screen name="Color" component={ColorScreen}/>
+            <Stack.Screen name="Square" component={SquareScreen}/>
+            <Stack.Screen name="Text" component={TextScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
